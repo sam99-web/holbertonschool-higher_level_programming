1a0c7dbaaf418a7951606a31f5f430e116891791
sam99-web <keishsam99@gmail.com>
2026-01-15 22:53:15 +0000

    Move python-hello_world from another repo

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "holberton School"
+print(str * 3)
+print(str[:9])
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str + str
+str = str + "\n" + str[:6]
+print(str)
