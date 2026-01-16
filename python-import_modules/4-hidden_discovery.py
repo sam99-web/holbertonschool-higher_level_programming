cbf4497371c6a1f6b9961f2ea1f03d9935fb4aba
sam99-web <keishsam99@gmail.com>
2026-01-16 21:11:06 +0000

    new modif add

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
-4 #!/usr/bin/python3
+#!/usr/bin/python3
+import hidden_4
+
+
 if __name__ == "__main__":
-    import hidden_4
-    
-    names = dir(hidden_4)
-    for name in sorted(names):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
