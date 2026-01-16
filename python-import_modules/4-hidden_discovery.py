9c60de4a34c81685f05117f5d19bbffec16dbd6e
sam99-web <keishsam99@gmail.com>
2026-01-16 21:54:09 +0000

    4modif commit add

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,8 @@
 import hidden_4
 
 if __name__ == "__main__":
-    for name in sorted(dir(hidden_4)):
+    
+    names = dir(hidden_4)
+    for name in sorted(names):
         if not name.startswith("__"):
             print(name)
