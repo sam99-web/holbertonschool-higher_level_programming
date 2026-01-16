72211e4c797ee9deac06f5510c3eb292777930a3
sam99-web <keishsam99@gmail.com>
2026-01-16 17:18:31 +0000

    new fille commit

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        # If character is lowercase, convert to uppercase
+        if 97 <= ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
