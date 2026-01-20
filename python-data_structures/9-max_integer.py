32d48bc973884372df68d762da6a3e557de901f1
sam99-web <keishsam99@gmail.com>
2026-01-20 01:27:23 +0000

    new commit add

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if list == " ":
+        return None
+
+    biggest = my_list[0]  #initialise of the fiirst elemence
+
+    for number in my_list:
+        if number > biggest:
+            biggest = number
+
+
+    return biggest
