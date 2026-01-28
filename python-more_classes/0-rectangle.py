22b39272510c6fb34f4a7bfa77f8970a825329f1
sam99-web <keishsam99@gmail.com>
2026-01-28 18:12:12 +0000

    new_modif file add to commit

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""this module difines an empty class Rectangle."""
+
+
 class Rectangle:
+    """An empty class that difines a rectangle."""
+
     pass
 r = Rectangle()
 print(r)
