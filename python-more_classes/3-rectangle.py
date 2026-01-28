b32d068f8e4ce520b43ac59e5b4786b2e1499b71
sam99-web <keishsam99@gmail.com>
2026-01-28 20:20:14 +0000

    new_modif add

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -40,6 +40,3 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width for _ in range(self.height)])
-
-    def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
