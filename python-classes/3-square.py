76873b80828b4e56d35bcb90667af919f29c383c
sam99-web <keishsam99@gmail.com>
2026-01-29 09:58:37 +0000

    publicmethod

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
 class Square:
+    """
+    This class defines a square with a private size attribute.
+    """
+
     def __init__(self, size=0):
-        # Vérification du type
+        """
+        Initialise a new Square instance.
+
+        Args:
+        size (int) the size of the square.
+
+        Raise:
+        TypeError (size must be an integer)
+        ValueError (size must be >=0)
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        # Vérification de la valeur
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        # Attribut privé
-        self.__size = size
+        if size <0:
+            raise ValueError("size must be >=0")
+        self.__size = size        
 
-    # Méthode publique pour calculer l'aire
+    """ Public intance methode that return the square area."""
     def area(self):
         return self.__size ** 2
