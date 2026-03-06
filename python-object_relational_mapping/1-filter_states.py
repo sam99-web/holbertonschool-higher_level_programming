29912b5d19039c12959a78076fb0f916053a843d
sam99-web <keishsam99@gmail.com>
2026-03-06 17:03:55 +0000

    lists all statst names

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module that lists all states from a MySQL database.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database):
+    """
+    Connects to the MySQL database and prints all states ordered by id.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    """
+    Gets arguments and calls the function.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
+
